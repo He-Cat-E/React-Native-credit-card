de7733f57e9ccf9a713238a1073158b856eab986
unknown <jasondavis@gmail.com>
2024-01-16 04:43:09 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# React-Native-credit-card
-React -Native project that implement interface between Phone and device.
+# React Native Credit Card 💳
+
+### A better credit card form for React Native
+
+React native port of the display of Card by [@jessepollak](https://github.com/jessepollak/card)
+
+![rn-credit-card](https://cdn.rawgit.com/sonnylazuardi/react-native-credit-card/master/demo.gif) ![rn-ios](http://i.giphy.com/l0MYFGQxfygHtBN84.gif)
+
+#### Demo
+
+[https://exp.host/@sonnylazuardi/react-native-credit-card-demos](https://exp.host/@sonnylazuardi/react-native-credit-card-demos)
+
+#### Background
+
+This is a react native port of Card. There is a [react port of Card](https://github.com/JohnyDays/react-credit-card), but it still renders on browser DOM (which is not as good as native view), so this library will take advantage of both.
+
+#### Goals
+
+- Look like Card.
+- Provide a react interface for displaying credit card information.
+
+#### Design Resources
+
+I got the design resources from the [Ramakrishna V's dribbble](https://dribbble.com/shots/2177105-Checkout-Flow-Card)
+
+![design](https://d13yacurqjgara.cloudfront.net/users/484057/screenshots/2177105/checkout_generic.gif)
+
+
+#### Usage
+
+- `npm install --save react-native-credit-card`
+- In your react native script add these lines
+
+```jsx
+import CreditCard from 'react-native-credit-card';
+
+
+<CreditCard
+    type={this.state.type}
+    imageFront={require('./images/card-front.png')}
+    imageBack={require('./images/card-back.png')}
+    shiny={false}
+    bar={false}
+    focused={this.state.focused}
+    number={this.state.number}
+    name={this.state.name}
+    expiry={this.state.expiry}
+    cvc={this.state.cvc}/>
+```
+
+### Available props
+
+- `cvc`: number(max 4 characters, will cut the rest)(The "/" will be automatically added)
+- `name`: string (max 2 lines)
+- `number`: credit card number(max 16 characters, will cut the rest)
+- `expiry`: number(max 4 characters, will cut the rest)
+- `imageFront`: image(image file for the card front card background -- with require)
+- `imageBack`: image(image file for the card back card background -- with require)
+- `shiny`: boolean(show sticker)
+- `bar`: boolean(show bar on the back)
+- `focused`: one of the above fields
+  - type
+  - name
+  - number
+  - expiry
+  - cvc (Upon focusing cvc, the form will be rotated until this is changed.)
+  - null
+- `type`: Available in case you need to explicitly specify the card type. In most cases this will be inferred for you as the user fills the form, so you won't need to use it. One of the following:
+  - dankort
+  - discover
+  - mastercard
+  - visa
+  - amex
+-`fullNameText`: Change the default text for FULL NAME
+-`showExpiryBeforeLabel`: boolean(show the MONTH/YEAR text)
+-`showExpiryAfterLabel`: boolean(show the VALID THRU text)
+-`expiryBeforeText`: Change the default text for MONTH/YEAR
+-`expiryAfterText`: Change the default text for VALID THRU
+
+#### Todo
+
+- Automated tests
+- More credit card type variations
+
+#### Examples
+
+todo.
+
+Most of the credit goes to [@jessepollak](http://github.com/jessepollak) and [@JohnyDays](http://github.com/JohnyDays) for creating and mantaining [Card](http://github.com/jessepollak/card) and [React Credit Card](https://github.com/JohnyDays/react-credit-card), I just ported it over to React Native.
+
+
---
+++ index.js
@@ -0,0 +1,354 @@
+'use strict';
+
+import React from 'react';
+const {
+    PropTypes,
+    Component
+} = React;
+
+import Payment from 'payment';
+import FlipCard from 'react-native-flip-card';
+
+import {
+    View,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Platform,
+    Image
+} from 'react-native';
+const images = require('./card-images');
+const validate = Payment.fns;
+
+class CreditCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: {
+                name:"unknown",
+                length: 16
+            }
+        }
+    }
+    getValue(name) {
+        return this[name]();
+    }
+    componentWillReceiveProps(nextProps) {
+        this.updateType(nextProps);
+    }
+    componentWillMount() {
+        this.updateType(this.props);
+    }
+    updateType(props) {
+
+        if (!props.number)
+            return this.setState({type: {name:"unknown", length: 16}});
+
+        var type = validate.cardType(props.number);
+        if (type) {
+            if (type === "amex") {
+                return this.setState({type: {name: type, length: 15}});
+            } else {
+                return this.setState({type: {name: type, length: 16}});
+            }
+        }
+
+        return this.setState({type: {name: "unknown", length: 16}});
+    }
+    number() {
+        if (!this.props.number) {
+            var string = "";
+        } else {
+            var string = this.props.number.toString();
+        }
+
+        const maxLength = this.state.type.length;
+
+        if (string.length > maxLength) string = string.slice(0, maxLength);
+
+        while (string.length < maxLength) {
+            string += "•"
+        }
+
+        if (this.state.type.name === "amex") {
+            const space_index1 = 4;
+            const space_index2 = 10;
+
+            string = string.substring(0, space_index1) + " " + string.substring(space_index1, space_index2) + " " + string.substring(space_index2);
+        } else {
+            const amountOfSpaces = Math.ceil(maxLength/4);
+
+            for (var i = 1; i <= amountOfSpaces; i++) {
+                var space_index = (i * 4 + (i - 1));
+                string = string.slice(0, space_index) + " " + string.slice(space_index)
+            }
+        }
+
+        return string;
+    }
+    name() {
+        if (this.props.name === "") {
+            return this.props.fullNameText.toUpperCase() || "FULL NAME";
+        } else {
+            return this.props.name.toUpperCase();
+        }
+    }
+    expiry() {
+        if (this.props.expiry === "") {
+            return "••/••";
+        } else {
+            var expiry = this.props.expiry.toString();
+
+            const expiryMaxLength = 6;
+
+            if (expiry.match(/\//))
+                expiry = expiry.replace("/", "");
+
+            if (!expiry.match(/^[0-9]*$/))
+                return "••/••";
+
+            while (expiry.length < 4) {
+                expiry += "•";
+            }
+
+            expiry = expiry.slice(0, 2) + "/" + expiry.slice(2, expiryMaxLength);
+        }
+
+        return expiry;
+    }
+
+    cvc() {
+        if (this.props.cvc == null) {
+            return "•••"
+        } else {
+            return (this.props.cvc.toString().length <= 4) ? this.props.cvc : this.props.cvc.toString().slice(0, 4);
+        }
+    }
+
+    render() {
+        const isAmex = this.state.type && this.state.type.name === "amex";
+        const cardStyle = [styles.container, {width: this.props.width, height: this.props.height, backgroundColor: this.props.bgColor}, this.props.style];
+        const {
+            showExpiryAfterLabel = true,
+            showExpiryBeforeLabel = true,
+            expiryBeforeText,
+            expiryAfterText,
+        } = this.props;
+
+        return (
+            <View style={cardStyle}>
+                <FlipCard
+                    style={[styles.container, {width: this.props.width, height: this.props.height, backgroundColor: this.props.bgColor}, this.props.style]}
+                    friction={6}
+                    perspective={1000}
+                    flipHorizontal={true}
+                    flipVertical={false}
+                    flip={this.props.focused === 'cvc'}
+                    clickable={this.props.clickable}
+                    onFlipped={(isFlipped)=>{console.log('isFlipped', isFlipped)}}
+                    >
+                    <View style={[styles.front, {width: this.props.width, height: this.props.height}]}>
+                        {this.props.imageFront ?
+                            <Image source={this.props.imageFront} style={[styles.bgImage, {width: this.props.width, height: this.props.height}]} />
+                            : null}
+                        <View style={styles.lower}>
+                            {this.props.shiny ?
+                                <View style={styles.shinyFront} />
+                                : null}
+                            <Image
+                                 style={styles.logo}
+                                 source={{uri: images[this.props.type ? this.props.type : this.state.type.name]}}
+                            />
+                            {isAmex ?
+                                <View style={styles.cvcFront}>
+                                    <Text style={styles.text}>{this.getValue("cvc")}</Text>
+                                </View>
+                                : null}
+                            <View style={styles.info}>
+                                <View style={styles.number}><Text style={styles.textNumber}>{this.getValue("number")}</Text></View>
+                                <View style={styles.rowWrap}>
+                                    <View style={styles.name}><Text style={styles.textName}>{this.getValue("name")}</Text></View>
+                                    {showExpiryAfterLabel &&
+                                        <View style={styles.validthru}>
+                                            <Text style={styles.textValidThru}>{expiryAfterText || 'VALID THRU'}</Text>
+                                        </View>
+                                    }
+                                    <View
+                                        style={styles.expiry}
+                                        data-before={this.props.expiryBefore}
+                                        data-after={this.props.expiryAfter}>
+                                        {showExpiryBeforeLabel && 
+                                            <Text style={styles.textSmall}>{expiryBeforeText || 'MONTH/YEAR'}</Text>
+                                        }
+                                        <Text style={styles.textExpiry}>{this.getValue("expiry")}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={[styles.back, {width: this.props.width, height: this.props.height}]}>
+                        {this.props.imageBack ?
+                            <Image source={this.props.imageBack} style={[styles.bgImage, {width: this.props.width, height: this.props.height}]} />
+                            : null}
+                        {this.props.bar ?
+                            <View style={styles.bar}/>
+                            : null}
+                        <View style={styles.cvc}><Text style={styles.textCvc}>{this.getValue("cvc")}</Text></View>
+                        {this.props.shiny ?
+                            <View style={styles.shinyBack} data-after={this.props.shinyAfterBack}/>
+                            : null}
+                    </View>
+                </FlipCard>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 8,
+        borderWidth: 0,
+        flex: null,
+    },
+    logo: {
+        height: 35,
+        width: 57,
+        position: 'absolute',
+        top: 20,
+        right: 20
+    },
+    text: {
+        color: '#fff'
+    },
+    bgImage: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        borderRadius: 8
+    },
+    lower: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        paddingVertical: 20,
+        paddingHorizontal: 20
+    },
+    expiry: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+    },
+    rowWrap: {
+        flexDirection: 'row',
+    },
+    name: {
+        flex: 2,
+        justifyContent: 'center'
+    },
+    validthru: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+    },
+    textValidThru: {
+        fontSize: 8,
+        color: '#ddd',
+        fontWeight: '900',
+        backgroundColor: 'transparent',
+    },
+    textSmall: {
+        fontSize: 8,
+        color: '#ddd',
+        fontWeight: '900',
+        backgroundColor: 'transparent',
+    },
+    textNumber: {
+        color: '#fff',
+        fontSize: 21,
+        textAlign: 'center',
+        marginBottom: 10,
+        backgroundColor: 'transparent',
+    },
+    textName: {
+        color: '#fff',
+        fontSize: 14,
+        backgroundColor: 'transparent',
+    },
+    textExpiry: {
+        color: '#fff',
+        fontSize: 16,
+        backgroundColor: 'transparent',
+    },
+    front: {
+        flex: 1
+    },
+    back: {
+        flex: 1
+    },
+    cvc: {
+        width: 45,
+        height: 30,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 15,
+        top: 76
+    },
+    textCvc: {
+        color: '#000',
+        fontSize: 18
+    },
+    info: {
+        flex: 1,
+    },
+    shinyFront: {
+        backgroundColor: '#ddd',
+        borderRadius: 8,
+        width: 50,
+        height: 40,
+        position: 'absolute',
+        top: 15,
+        left: 20
+    },
+    shinyBack: {
+        backgroundColor: '#ddd',
+        borderRadius: 8,
+        width: 50,
+        height: 40,
+        position: 'absolute',
+        bottom: 15,
+        left: 20
+    },
+    bar: {
+        height: 40,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 30,
+        backgroundColor: '#000'
+    }
+});
+
+CreditCard.defaultProps = {
+    number: null,
+    cvc: null,
+    name: '',
+    expiry: '',
+    focused: null,
+    expiryBefore: 'month/year',
+    expiryAfter: 'valid thru',
+    shinyAfterBack: '',
+    type: null,
+    width: 300,
+    height: 180,
+    bgColor: '#191278',
+    clickable: true,
+};
+
+CreditCard.CardImages = images;
+
+module.exports = CreditCard;
+
